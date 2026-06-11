d06f4028db0adf81a984014eb86edb1cb99c2dc3
Safaralievna <farangizshaykulova06@gmail.com>
2026-06-11 15:27:38 +0500

    Edit code

--- frontend/src/pages/Login.tsx
+++ frontend/src/pages/Login.tsx
@@ -113,7 +113,7 @@ export const Login: React.FC = () => {
             <div className="space-y-5">
               <div className="relative group">
                 <Input 
-                  label="Elektron pochta"
+                  label="Elektron pochta test"
                   type="email" 
                   placeholder="name@company.uz"
                   error={errors.email?.message}
